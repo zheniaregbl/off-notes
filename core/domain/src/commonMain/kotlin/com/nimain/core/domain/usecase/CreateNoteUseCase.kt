563d8287185ddac2cf04f84f0817844d4bc08476
zheniaregbl <evgenieregbl@gmail.com>
2026-08-17 22:54:39 +0300

    refactor(core:domain): changed creating and saving note use cases

--- core/domain/src/commonMain/kotlin/com/nimain/core/domain/usecase/CreateNoteUseCase.kt
+++ core/domain/src/commonMain/kotlin/com/nimain/core/domain/usecase/CreateNoteUseCase.kt
@@ -3,5 +3,5 @@ package com.nimain.core.domain.usecase
 import com.nimain.core.domain.repository.NoteRepository
 
 class CreateNoteUseCase(private val repository: NoteRepository) {
-    suspend operator fun invoke(title: String, content: String) = repository.createNote(title, content)
+    suspend operator fun invoke() = repository.createNote()
 }
--- core/domain/src/commonMain/kotlin/com/nimain/core/domain/usecase/SaveNoteUseCase.kt
+++ core/domain/src/commonMain/kotlin/com/nimain/core/domain/usecase/SaveNoteUseCase.kt
@@ -3,5 +3,6 @@ package com.nimain.core.domain.usecase
 import com.nimain.core.domain.repository.NoteRepository
 
 class SaveNoteUseCase(private val repository: NoteRepository) {
-    suspend operator fun invoke(id: String, content: String) = repository.saveNote(id, content)
+    suspend operator fun invoke(id: String, title: String, content: String) =
+        repository.saveNote(id, title, content)
 }
